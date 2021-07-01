982af3421aeaafed8b5041faf62e4702fffcf85c
Luiz Eduardo <l.eduardo8316@gmail.com>
2021-07-01 13:48:56 -0300

    create new date object imutable

--- client/js/app/models/Negotiation.js
+++ client/js/app/models/Negotiation.js
@@ -4,21 +4,23 @@ class Negotiation {
   _quantity;
 
   constructor(data = new Date(), value = 0.0, quantity = 1) {
-    this._data = data;
+    this._data = new Date(data.getTime()); //cria um novo objeto de date a partir dos miliseconds da data informada pelo usuário
     this._value = value;
     this._quantity = quantity;
+    Object.freeze(this);
+    Object.freeze(this._data);
   }
 
   get data() {
-    return this._data
+    return new Date(this._data.getTime()); //dessa forma qualquer alteração feita diretamente na data não vai ser válida pois está sendo criado uma nova referência de data
   }
 
   get value() {
-    return this._value
+    return this._value;
   }
 
   get quantity() {
-    return this._quantity
+    return this._quantity;
   }
 
   getVolume(_value, _quantity) {
